d2ef7a599c28a294e1d27668ca8fa252af0a1b01
Nidhish Puthiyadath <nidhish.nambiar@gmail.com>
2018-06-21 11:26:52 -0700

    Update test.js
    Add async test back

--- src/test.js
+++ src/test.js
@@ -17,13 +17,13 @@ describe('Sample Test', () => {
             'hello'.should.equal('hello')
         })
     )
-    //
-    // describe('Test Library', () =>
-    //     it('should run after 3 seconds', () =>
-    //         lib.resolveAfterFewSeconds(3000).then((result) => {
-    //             expect(result).to.equal('This was called after 3 seconds') //using expect
-    //             result.should.equal('This was called after 3 seconds') //using should
-    //         }).done
-    //     )
-    // )
+    
+     describe('Test Library', () =>
+         it('should run after 3 seconds', () =>
+             lib.resolveAfterFewSeconds(3000).then((result) => {
+                 expect(result).to.equal('This was called after 3 seconds') //using expect
+                 result.should.equal('This was called after 3 seconds') //using should
+             }).done
+         )
+     )
 })
